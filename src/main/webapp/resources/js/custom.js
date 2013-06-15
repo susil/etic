7fb5e2016ade9368511368d6b868476b241ecef9
susil <srayamajhi@yahoo.com>
2013-06-15 08:32:49 -0700

    tabledata added

--- src/main/webapp/resources/js/custom.js
+++ src/main/webapp/resources/js/custom.js
@@ -9,23 +9,65 @@
  * 
  */
 var urlHolder = new Object();
+var oTable = null;
+
+function initTable ()
+{
+  return $('#tableSystems').dataTable( {
+		//"sAjaxSource": 'http://localhost:8080/myapp/systems/systemtable',
+		"bRetrieve ": true
+	} ); 
+  
+}
+
+function reloadtable()
+{
+	oTable = $('#tableSystems').dataTable( {
+		"bProcessing": true,
+		"sAjaxSource": 'http://localhost:8080/myapp/systems/systemtable',
+		"bDestroy": true,
+		"bAutoWidth" : false, 
+		 "aoColumns":[
+						{"sWidth": '10%', "mDataProp": "id", bSearchable: false, bSortable: false},
+						{"sWidth": '25%', "mDataProp": "title"},
+						{"sWidth": '40%', "mDataProp": "description"}
+			],
+			"aoColumnDefs": [ {
+			      "aTargets": [ 0 ],
+			      //"mData": "download_link",
+			      "mRender": function ( data, type, full ) {
+			        //return '<a href="'+data+'">Download</a>';
+		        return '<input type="radio" name="index" id="index" value="'+data+'"/>' +
+		        '<input type="hidden" name="id" id="id" value="' + data + '"/>';
+			      }
+			    } ]	
+	} );
+	
+//	  $('#tableSystems').dataTable( {
+//		    "bFilter": false,
+//		    "bDestroy": true
+//		  } );
+	  
+	  //initTable();
+}
 
 function loadTable() {
-	//alert('loading...');
+	////#//alert('loading...');
+	reloadtable();
+	//initTable();
 	$.get(urlHolder.records, function(response) {
 		
-		
- 		$('#tableSystems').find('tbody').remove();
- 		
- 		for (var i=0; i<response.systems.length; i++) {
-			var row = '<tr>';
-			row += '<td><input type="radio" name="index" id="index" value="'+i+'"></td>';
-			row += '<td>' + response.systems[i].id + '</td>';
-			row += '<td>' + response.systems[i].title + '</td>';
-			row += '<td>' + response.systems[i].description + '</td>';
-			row += '</tr>';
-	 		$('#tableSystems').append(row);
- 		}
+// 		$('#tableSystems').find('tbody').remove();
+// 		
+// 		for (var i=0; i<response.systems.length; i++) {
+//			var row = '<tr>';
+//			row += '<td><input type="radio" name="index" id="index" value="'+i+'"></td>';
+//			row += '<td>' + response.systems[i].id + '</td>';
+//			row += '<td>' + response.systems[i].title + '</td>';
+//			row += '<td>' + response.systems[i].description + '</td>';
+//			row += '</tr>';
+//	 		$('#tableSystems').append(row);
+// 		}
  		
  		$('#tableSystems').data('model', response.systems);
 		toggleForms('hide');
@@ -33,14 +75,16 @@ function loadTable() {
 }
 
 function loadConsumerTable() {
-	//alert('loading consumers...');
+	////#//alert('loading consumers...');
 	var selected = $('input:radio[name=index]:checked').val();
 	//$('#sourceId').val( $('#tableSystems').data('model')[selected].id );
-	
+	//#//alert('selected='+selected);
 	$.get(urlHolder.consumers, 
 			
 			{
-			sourceId: $('#tableSystems').data('model')[selected].id
+			//#//sourceId: $('#tableSystems').data('model')[selected].id
+			//$('#id').val()
+				sourceId:selected
 			},
 			function(response) {
 		
@@ -66,21 +110,21 @@ function loadConsumerTable() {
 
 function submitNewRecord() {
 	
-	//alert('Step 2:'+ urlHolder.add+':'+$('#id').val()+':'+$('#title').val()+':'+$('#description').val());
+	////#//alert('Step 2:'+ urlHolder.add+':'+$('#id').val()+':'+$('#title').val()+':'+$('#description').val());
 	
 	$.post(urlHolder.add, {
-			id: $('#id').val(),
-			title: $('#title').val(),
-			description: $('#description').val()
+			id: $('#newId').val(),
+			title: $('#newTitle').val(),
+			description: $('#newDescription').val()
 		}, 
 		function(response) {
 			if (response != null) {
 				loadTable();
 				toggleForms('hide'); ;
 				toggleCrudButtons('show');
-				alert('Success! Record has been added.');
+				//#//alert('Success! Record has been added.');
 			} else {
-				alert('Failure! An error has occurred!');
+				//#//alert('Failure! An error has occurred!');
 			}
 		}
 	);	
@@ -90,14 +134,15 @@ function submitDeleteRecord() {
 	var selected = $('input:radio[name=index]:checked').val();
 	
 	$.post(urlHolder.del, {
-			id: $('#tableSystems').data('model')[selected].id
+			//id: $('#tableSystems').data('model')[selected].id
+			id:selected
 		}, 
 		function(response) {
 			if (response == true) {
 				loadTable(urlHolder.records);
-				alert('Success! Record has been deleted.');
+				//#//alert('Success! Record has been deleted.');
 			} else {
-				alert('Failure! An error has occurred!');
+				//#//alert('Failure! An error has occurred!');
 			}
 		}
 	);
@@ -107,17 +152,18 @@ function submitUpdateRecord() {
 	$.post(urlHolder.edit, {
 			id: $('#editId').val(),
 			title: $('#editTitle').val(),
-			description: $('#editDescription').val(),
-			role: $('#editRole').val()
+			description: $('#editDescription').val()
 		}, 
 		function(response) {
-			if (response == true) {
+			////#//alert(response.id );
+			//if (response == true) {
+			if (response.id == $('#editId').val() ) {
 				loadTable();
 				toggleForms('hide'); ;
 				toggleCrudButtons('show');
-				alert('Success! Record has been edited.');
+				//#//alert('Success! Record has been edited.');
 			} else {
-				alert('Failure! An error has occurred!');
+				//#//alert('Failure! An error has occurred!');
 			}
 		}
 	);
@@ -134,9 +180,9 @@ function submitConnectRecord() {
 				loadTable();
 				toggleForms('hide'); ;
 				toggleCrudButtons('show');
-				alert('Connect Success! Record has been edited.');
+				//#//alert('Connect Success! Record has been edited.');
 			} else {
-				alert('Connect Failure! An error has occurred!');
+				//#//alert('Connect Failure! An error has occurred!');
 			}
 		}
 	);
@@ -145,7 +191,7 @@ function submitConnectRecord() {
 function hasSelected() {
 	var selected = $('input:radio[name=index]:checked').val();
 	if (selected == undefined) { 
-		alert('Select a record first!');
+		//#//alert('Select a record first!');
 		return false;
 	}
 	
@@ -154,14 +200,47 @@ function hasSelected() {
 
 function fillEditForm() {
 	var selected = $('input:radio[name=index]:checked').val();
-	$('#editId').val( $('#tableSystems').data('model')[selected].id );
-	$('#editTitle').val( $('#tableSystems').data('model')[selected].title );
-	$('#editDescription').val( $('#tableSystems').data('model')[selected].description );
+	//#//alert(selected+":");
+	
+	$.get(urlHolder.aRecord+selected, 
+			
+//			{
+//				sourceId:selected
+//			},
+			function(aRecordResponse) {
+		
+		$('#editId').val( aRecordResponse.id );
+		$('#editTitle').val( aRecordResponse.title );
+		$('#editDescription').val( aRecordResponse.description );
+		
+ 	});
+	
+	
+    
+
+    
+
 }
 
 function fillConnectForm() {
 	var selected = $('input:radio[name=index]:checked').val();
-	$('#sourceId').val( $('#tableSystems').data('model')[selected].id );
+	//$('#sourceId').val( $('#tableSystems').data('model')[selected].id );
+	$('#sourceId').val( selected );
+	
+    var jsonList;
+    var listItems= "";
+    $.get(urlHolder.records, function(response) { 
+
+    for (var i = 0; i < response.systems.length; i++){
+        listItems+= "<option value='" + response.systems[i].id + "'>" + response.systems[i].title + "</option>";
+      }
+      $("#destinationId").html(listItems);
+
+      
+    });
+    
+    
+
 }
 
 function resetNewForm() {
@@ -181,7 +260,8 @@ function toggleForms(id) {
 		$('#newForm').hide();
 		$('#editForm').hide();
 		$('#connectForm').hide();
-		//alert('hide all forms...');
+		$("#divConsumers").hide();
+		////#//alert('hide all forms...');
 		
 	} else if (id == 'new') {
  		resetNewForm();
@@ -204,7 +284,7 @@ function toggleForms(id) {
  		$('#newForm').hide();
  		$('#editForm').hide();
  		$('#connectForm').hide();
- 		$("divConsumers").show();
+ 		$("#divConsumers").show();
 	}
 }
 
